016ce0e2529c2194025bc13dac0175d53ae5d20f
Alex Dong <team.magiccart@gmail.com>
2022-03-12 15:59:02 +0700

    Init commit

---
+++ command.sh
@@ -0,0 +1,32 @@
+I. Docs
+
+Video: https://www.youtube.com/playlist?list=PLwJr0JSP7i8BRFtZ56gFmg6NVX0sMx980
+
+II. WLS note: 
+
+1. Access terminal wls or run program linu from Command Prompt
+	wls -l : show list wls install in windows
+	wls : access Ubuntu command
+
+	wls nano : open app nano in linux from Command Prompt windows
+
+	Type \\wsl$Ubuntu in windows explore : acces Ubuntu file from window
+
+2. Run command in terminal wls
+
+	exit : exit wls terminal
+
+	explore.exe . : open windows explore from wls terminal
+
+	cd /mnt/c/ : Access volume C windows
+
+3. Open file in system WLS with Visual Code
+
+	https://www.youtube.com/watch?v=QeNjUfv0WGc&list=PLwJr0JSP7i8BRFtZ56gFmg6NVX0sMx980&index=5
+
+4. Docker in WLS2
+
+III. Terminal windows
+
+	Install Terminal windows: https://docs.microsoft.com/en-us/windows/terminal/
+	Alt + Shift + "+": to open multi terminal
